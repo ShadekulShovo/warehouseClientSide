7da77f6643b0a1203eda6dc92b6bd2482fbb5efa
ShadekulShovo <shadekuljamalpur.bd21@gmail.com>
2022-05-08 12:28:37 +0600

    footer added

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -180,6 +180,38 @@ const Home = () => {
           </div>
         </Slider>
       </div>
+      <div className="container-fluid footer">
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-muted">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-muted">
+                Category
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-muted">
+                Inventory
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-muted">
+                Blog
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+          <p class="text-center text-muted">© 2021 Electronics Mania, Inc</p>
+        </footer>
+      </div>
     </div>
   );
 };
